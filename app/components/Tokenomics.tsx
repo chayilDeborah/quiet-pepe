9f5fa6244c025280e1c3975e62bdfb6181c2768c
chayilDeborah <aniyikayeolamide3@gmail.com>
2024-04-26 04:20:36 +0100

    feat: mobile responsiveness

--- app/components/Tokenomics.tsx
+++ app/components/Tokenomics.tsx
@@ -9,20 +9,20 @@ const Tokenomics = () => {
                     <div className='lg:max-w-[90rem]  w-[95%] mx-auto lg:w-full'>
                         <div className=''>
                             <h1 className='who font-bold my-[24px] text-center pt-[100px] mb-[60px] '>TOKENOMICS</h1>
-                            <div className='lg:flex justify-between '>
+                            <div className='flex flex-col-reverse lg:flex-row  justify-between '>
                                 <div className=' video-container'>
-                                    <video loop className="lg:w-[566px] h-[616px] " autoPlay muted>
+                                    <video loop className="lg:w-[566px] h-[437px] lg:h-[616px] " autoPlay muted>
                                         <source src="/tokenomics.mp4" type="video/mp4" />
                                     </video>
                                 </div>
-                                <div className='mx-auto'>
-                                    <h1 className='border-[2px] text-[27px] flex justify-between border-white px-[15px] py-[15px] rounded-[16px] mt-[30px] '>Symbol<span className='ml-[140px] '>GONZO</span></h1>
-                                    <h1 className='border-[2px] text-[27px] flex justify-between border-white px-[15px] py-[15px] rounded-[16px] mt-[30px] '>Tax<span className='ml-[140px] '>0/0</span></h1>
-                                    <h1 className='border-[2px] text-[27px] flex justify-between border-white px-[15px] py-[15px] rounded-[16px] mt-[30px] '>Liquidity<span className='ml-[90px] lg:ml-[140px] '> Locked</span></h1>
-                                    <h1 className='border-[2px] text-[27px] flex justify-between border-white px-[15px] py-[15px] rounded-[16px] mt-[30px] '>Total Supply<span className='ml-[30px] lg:ml-[140px] '> 100,000,000,000</span></h1>
-                                    <div className='border-[2px] text-[27px] px-[15px] py-[15px] rounded-[16px] mt-[30px] '>
+                                <div className='mx-auto lg:w-[525px] w-[95%] '>
+                                    <h1 className='border-[2px] text-[19px] lg:text-[27px] flex justify-between border-white px-[15px] py-[15px] rounded-[16px] mt-[30px] '>Symbol<span className='ml-[140px] '>GONZO</span></h1>
+                                    <h1 className='border-[2px] text-[19px] lg:text-[27px] flex justify-between border-white px-[15px] py-[15px] rounded-[16px] mt-[30px] '>Tax<span className='ml-[140px] '>0/0</span></h1>
+                                    <h1 className='border-[2px] text-[19px] lg:text-[27px] flex justify-between border-white px-[15px] py-[15px] rounded-[16px] mt-[30px] '>Liquidity<span className='ml-[90px] lg:ml-[140px] '> Locked</span></h1>
+                                    <h1 className='border-[2px] text-[19px] lg:text-[27px] flex justify-between border-white px-[15px] py-[15px] rounded-[16px] mt-[30px] '>Total Supply<span className='ml-[30px] lg:ml-[140px] '> 100,000,000,000</span></h1>
+                                    <div className='border-[2px] text-[19px] lg:text-[27px] px-[15px] py-[15px] rounded-[16px] mt-[30px] '>
                                         <h1 className='text-center border-white '>TOKEN ADDRESS</h1>
-                                        <p className='text-[22px] '>0x0000000000000000000000000</p>
+                                        <p className='text-[22px] '>0x00000000000000000000000</p>
                                     </div>
                                 </div>
                             </div>
