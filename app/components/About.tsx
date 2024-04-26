3f0a8aaf08e327af018cf353ab79473f2c6ea5a8
chayilDeborah <aniyikayeolamide3@gmail.com>
2024-04-26 04:22:02 +0100

    feat: community responsiveness

--- app/components/About.tsx
+++ app/components/About.tsx
@@ -2,19 +2,19 @@ import React from 'react'
 
 const About = () => {
     return (
-        <main className='flex justify-center w-full mt-[100px] text-white ' id='about'>
-            <section className=' w-[90%] lg:w-full'>
-                <div className='flex justify-between'>
+        <main className='flex justify-center w-full mt-[350px] lg:mt-[100px] text-white ' id='about'>
+            <section className='w-full'>
+                <div className='flex flex-col-reverse lg:flex-row justify-between'>
                     <section>
                         <div className=' video-container'>
-                            <video loop className="lg:w-[566px] h-[616px] " autoPlay muted>
+                            <video loop className="lg:w-[566px] w-[90%] mx-auto lg:mx-[0px] h-[420px] lg:h-[616px] " autoPlay muted>
                                 <source src="/tv.mp4" type="video/mp4" />
                             </video></div>
                     </section>
-                    <section className='mt-[90px] ml-[30px] '>
-                        <h3 className='text-[18px] leading-[150%] font-bold tracking-[12.06px]  '>ABOUT $GONZO</h3>
-                        <h1 className='who font-bold my-[24px] '>WHO&apos;S GONZO?!</h1>
-                        <div className='w-[717px] text-[24px] leading-[180%]  '>This mischievous monkey is notorious for his light-fingered antics and his uncanny ability to outwit even the most vigilant of netizens.
+                    <section className='mt-[90px] lg:ml-[30px] '>
+                        <h3 className='text-[18px] leading-[150%] font-bold tracking-[12.06px]  text-center lg:text-left   '>ABOUT $GONZO</h3>
+                        <h1 className='who font-bold mt-[17px] lg:my-[24px] text-center lg:text-left  '>WHO&apos;S GONZO?!</h1>
+                        <div className='lg:w-[717px] lg:text-[24px] text-[18px] text-center lg:text-leftleading-[180%]  '>This mischievous monkey is notorious for his light-fingered antics and his uncanny ability to outwit even the most vigilant of netizens.
 
                           <p className='mt-[20px]'>  With a twinkle in his eye and a sly grin on his face, Gunzo is always on the lookout for his next score. Whether it&apos;s swiping a banana from a rival meme or pilfering pixels from unsuspecting artists, he&apos;s the ultimate digital delinquent.</p>
                         </div>
--- app/components/Community.tsx
+++ app/components/Community.tsx
@@ -7,12 +7,12 @@ import twit from '../assets/twit.svg'
 const Community = () => {
     return (
         <main className='flex justify-center text-white mt-[100px] '>
-            <div className='lg:max-w-[90rem]  w-[95%] mx-auto lg:w-full'>
-                <div className='flex justify-between '>
+            <div className='lg:max-w-[90rem] w-[95%] mx-auto lg:w-full'>
+                <div className='lg:flex justify-between '>
                     <section>
-                        <h3 className='join mt-[80px] '>JOIN OUR COMMUNITY</h3>
-                        <p className='w-[792px] text-[24px] leading-[180%] text-[#000] '>As Gunzo swings through the digital jungle, gathering memes and causing mischief, his gang of loyal followers grows stronger with each laugh shared. Together, we fuel our meme arsenal, amplifying the hilarity and spreading joy across cyberspace.</p>
-                        <div className='lg:flex w-[140px] mt-[40px] justify-between   '>
+                        <h3 className='join lg:mt-[80px]  text-center lg:text-left'>JOIN OUR COMMUNITY</h3>
+                        <p className='lg:w-[792px] text-[18px] lg:text-[24px] leading-[180%] text-center lg:text-left text-[#000] '>As Gunzo swings through the digital jungle, gathering memes and causing mischief, his gang of loyal followers grows stronger with each laugh shared. Together, we fuel our meme arsenal, amplifying the hilarity and spreading joy across cyberspace.</p>
+                        <div className='flex w-[140px] mt-[40px] justify-center lg:justify-between mx-auto lg:mx-[0px]  '>
                             <Link href="/#about" className=' '>
                                 <div className='w-[105px] h-[80px] rounded-[16px] tgjoin '>
                                     <Image src={tg} alt='telegram' className='w-[50px] ' />
@@ -27,7 +27,7 @@ const Community = () => {
                     </section>
                     <section>
                     <div className=' video-container'>
-                                    <video loop className="lg:w-[548px] h-[542px] " autoPlay muted>
+                                    <video loop className="lg:w-[548px] w-[90%] h-[542px] mx-auto lg:mx-[0px] " autoPlay muted>
                                         <source src="/money.mp4" type="video/mp4" />
                                     </video>
                                 </div>
