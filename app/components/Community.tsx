d49ad7623db4fac7b63624e86beca71825794bfc
chayilDeborah <aniyikayeolamide3@gmail.com>
2024-04-29 11:18:34 +0100

    feat: fix social links

--- app/components/Community.tsx
+++ app/components/Community.tsx
@@ -14,12 +14,12 @@ const Community = () => {
                         <h3 className='join lg:mt-[80px]  text-center lg:text-left' data-aos="fade-up" data-duration="1400">JOIN OUR COMMUNITY</h3>
                         <p className='lg:w-[695px] text-[17px] lg:text-[24px] leading-[180%] text-center lg:text-left text-white ' data-aos="fade-up" data-duration="1600">As Gonzo swings through the digital jungle, gathering memes and causing mischief, his gang of loyal followers grows stronger with each laugh shared. Together, we fuel our meme arsenal, amplifying the hilarity and spreading joy across cyberspace.</p>
                         <div className='flex w-[140px] mt-[65px] lg:mt-[40px] justify-center lg:justify-between mx-auto lg:mx-[0px]  ' data-aos="fade-up" data-duration="1800">
-                            <Link href="/https://t.me/gonzo_onchain" className=' '>
+                            <Link href="https://t.me/gonzo_onchain" className=' '>
                                 <div className='w-[105px] h-[80px] rounded-[16px] tgjoin '>
                                     <Image src={tg} alt='telegram' className='w-[50px] ' />
                                 </div>
                             </Link>
-                            <Link href="/https://x.com/gonzo_onchain">
+                            <Link href="https://x.com/gonzo_onchain">
                                 <div className='w-[105px] h-[80px] rounded-[16px] tgjoin ml-[40px] '>
                                     <Image src={twit} alt='twitter' className='w-[50px] ' />
                                 </div>
